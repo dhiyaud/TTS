fb58ff4e1b25094e8a6f10183a8ae22d14a8b9ae
Dhiya Ulhaq Dewangga <dhiya.dewangga@prosa.ai>
2023-08-04 13:33:10 +0700

    Update latest code

--- TTS/tts/layers/losses.py
+++ TTS/tts/layers/losses.py
@@ -169,7 +169,7 @@ class BCELossMasked(nn.Module):
 
     def __init__(self, pos_weight: float = None):
         super().__init__()
-        self.pos_weight = nn.Parameter(torch.tensor([pos_weight]), requires_grad=False)
+        self.register_buffer("pos_weight", torch.tensor([pos_weight]))
 
     def forward(self, x, target, length):
         """
@@ -195,16 +195,21 @@ class BCELossMasked(nn.Module):
             mask = sequence_mask(sequence_length=length, max_len=target.size(1))
             num_items = mask.sum()
             loss = functional.binary_cross_entropy_with_logits(
-                x.masked_select(mask), target.masked_select(mask), pos_weight=self.pos_weight, reduction="sum"
+                x.masked_select(mask),
+                target.masked_select(mask),
+                pos_weight=self.pos_weight.to(x.device),
+                reduction="sum",
             )
         else:
-            loss = functional.binary_cross_entropy_with_logits(x, target, pos_weight=self.pos_weight, reduction="sum")
+            loss = functional.binary_cross_entropy_with_logits(
+                x, target, pos_weight=self.pos_weight.to(x.device), reduction="sum"
+            )
             num_items = torch.numel(x)
         loss = loss / num_items
         return loss
 
 
-class DifferentailSpectralLoss(nn.Module):
+class DifferentialSpectralLoss(nn.Module):
     """Differential Spectral Loss
     https://arxiv.org/ftp/arxiv/papers/1909/1909.10302.pdf"""
 
@@ -339,7 +344,7 @@ class TacotronLoss(torch.nn.Module):
             self.criterion_ga = GuidedAttentionLoss(sigma=ga_sigma)
         # differential spectral loss
         if c.postnet_diff_spec_alpha > 0 or c.decoder_diff_spec_alpha > 0:
-            self.criterion_diff_spec = DifferentailSpectralLoss(loss_func=self.criterion)
+            self.criterion_diff_spec = DifferentialSpectralLoss(loss_func=self.criterion)
         # ssim loss
         if c.postnet_ssim_alpha > 0 or c.decoder_ssim_alpha > 0:
             self.criterion_ssim = SSIMLoss()
